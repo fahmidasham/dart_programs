01b12ca069dc217d504a33eb7ed145188003386f
fahmidasham <fahmidasai@gmail.com>
2023-09-25 11:28:41 +0530

    initial commit

---
+++ bin/CONTROL STATMENTS/nested_elseif.dart
@@ -0,0 +1,29 @@
+void main() {
+  String username = "admin";
+  String password = "1as";
+  int otp = 1234;
+  /// nested if
+  /// if (username=="Admin" && password=="1as") {
+  // print("successfully entered");
+  // if (otp ==1234) {
+  // print('u successful login');}}
+  // else {
+  // print ('ur email or password is incorrect');
+  // }
+  // }
+
+
+    ///nested else if
+if (username=="admin" && password=="1as") {
+print("successfully entered");
+if (otp ==124) {
+print('u successful login');}
+else {
+  print("otp is incorrect");
+}
+}
+
+else {
+print ('ur email or password is incorrect');
+}
+}
