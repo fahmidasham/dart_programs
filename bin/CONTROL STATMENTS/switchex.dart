badb0f44ca277719dbb69a5e66066074f775e401
fahmidasham <fahmidasai@gmail.com>
2023-12-31 12:40:09 +0530

    initial commit

---
+++ bin/CONTROL STATMENTS/switchex.dart
@@ -0,0 +1,19 @@
+void main(){
+  int size=(10);
+  switch(size){
+    case 1:
+  print("size is 4");
+    case 2:
+      print("is 5");
+    case 3:
+      print("is 6");
+    case 4:
+      print("size is 7");
+    case 5:
+      print("is 8");
+    case 6:
+      print("is 9");
+    default :
+    print("no size");
+  }
+  }
